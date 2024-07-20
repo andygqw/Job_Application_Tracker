ffe08a2407285fd629df0d4c9a304f03b06d3eba
andygqw <andygqwang@gmail.com>
2024-07-20 10:14:57 -0700

    added secrets to env, set ups

--- JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/controller/AuthController.java
+++ JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/controller/AuthController.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthController {
 
+
+    @GetMapping("/")
+    public String home(){
+
+        return "index";
+    }
+
     @GetMapping("/login")
     public String login(){
 
--- JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/service/CloudFlareD1Service.java
+++ JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/service/CloudFlareD1Service.java
@@ -1,5 +1,7 @@
 package com.gw.JobApplicationTracker.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -12,26 +14,38 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
+
 @Service
 public class CloudFlareD1Service {
 
     private static final Logger logger = LoggerFactory.getLogger(CloudFlareD1Service.class);
 
-    private static String END_POINT;
 
-    private static String BEARER_TOKEN;
+    @Value("${cf.account.id}")
+    private String account_id;
+
+    @Value("${cf.database.id}")
+    private String database_id;
+
+    @Value("${cf.database.token}")
+    private String database_token;
+
+    private static String END_POINT;
 
     private final WebClient webClient;
 
     public CloudFlareD1Service(){
 
-        END_POINT = "";
-        BEARER_TOKEN = "";
+        END_POINT = "https://api.cloudflare.com/client/v4/accounts/"+ 
+                        "account_id" + 
+                        "/d1/database/" + 
+                        "database_id" +
+                        "/raw";
 
         webClient = WebClient.builder()
                 .baseUrl(END_POINT)
                 .defaultHeader("Content-Type", "application/json")
-                .defaultHeader("Authorization", "Bearer " + BEARER_TOKEN)
+                .defaultHeader("Authorization", "Bearer " + "database_token")
                 .build();
     }
 
