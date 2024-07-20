f9c4237d8cb7668947cdd2716f68a8d004dd4f3c
andygqw <andygqwang@gmail.com>
2024-07-20 10:53:04 -0700

    fixed env issue

--- JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/service/CloudFlareD1Service.java
+++ JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/service/CloudFlareD1Service.java
@@ -1,8 +1,10 @@
 package com.gw.JobApplicationTracker.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import reactor.core.publisher.Mono;
@@ -14,12 +16,14 @@ import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 
+import jakarta.annotation.PostConstruct;
+
+
 
 @Service
 public class CloudFlareD1Service {
 
     private static final Logger logger = LoggerFactory.getLogger(CloudFlareD1Service.class);
-
 
     @Value("${cf.account.id}")
     private String account_id;
@@ -30,32 +34,40 @@ public class CloudFlareD1Service {
     @Value("${cf.database.token}")
     private String database_token;
 
-    private static String END_POINT;
+    private String END_POINT;
 
-    private final WebClient webClient;
+    private WebClient webClient;
 
-    public CloudFlareD1Service(){
-
-        END_POINT = "https://api.cloudflare.com/client/v4/accounts/"+ 
-                        "account_id" + 
-                        "/d1/database/" + 
-                        "database_id" +
-                        "/raw";
-
+    public CloudFlareD1Service() {
         webClient = WebClient.builder()
-                .baseUrl(END_POINT)
                 .defaultHeader("Content-Type", "application/json")
-                .defaultHeader("Authorization", "Bearer " + "database_token")
                 .build();
     }
+
+    @PostConstruct
+    private void init() {
+
+        END_POINT = "https://api.cloudflare.com/client/v4/accounts/" + 
+                    account_id + 
+                    "/d1/database/" + 
+                    database_id +
+                    "/raw";
+
+        this.webClient = WebClient.builder()
+                .baseUrl(END_POINT)
+                .defaultHeader("Content-Type", "application/json")
+                .defaultHeader("Authorization", "Bearer " + database_token)
+                .build();
+    }
+
 
     public Mono<ResponseEntity<String>>  GetQueryMono (String query){
 
         String body = "{\n  \"params\": [\n ],\n  \"sql\": \"" + query + "\"\n}";
 
         return webClient.post()
-                .bodyValue(body)
-                .retrieve()                
-                .toEntity(String.class);                
+        .bodyValue(body)
+        .retrieve()                
+        .toEntity(String.class);                
     }
 }
