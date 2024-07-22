b535e33d1bd6d9f3535c93cc0b8e967ad4065ed9
andygqw <andygqwang@gmail.com>
2024-07-22 12:57:43 -0700

    added dashboard datas and further security changes

--- JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/controller/dashboardController.java
+++ JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/controller/dashboardController.java
@@ -1,15 +1,19 @@
 package com.gw.JobApplicationTracker.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import reactor.core.publisher.Mono;
 
 @Controller
 public class DashboardController {
 
 
     @GetMapping("/dashboard")
-    public String dashboard(){
-
-        return "dashboard";
+    public Mono<String> dashboard(Authentication authentication, Model model) {
+        model.addAttribute("username", authentication.getName());
+        return Mono.just("dashboard");
     }
 }
