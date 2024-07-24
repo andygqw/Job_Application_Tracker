816efb194b898ef86f361492b8372b5e0ce68e49
andygqw <andygqwang@gmail.com>
2024-07-24 14:31:23 -0700

    Fixed not provider issue when calling formlogin with custom authenticationmanager

--- JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/component/JwtAuthenticationSuccessHandler.java
+++ JobApplicationTracker/src/main/java/com/gw/JobApplicationTracker/component/JwtAuthenticationSuccessHandler.java
@@ -1,5 +1,7 @@
 package com.gw.JobApplicationTracker.component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,12 +11,16 @@ import org.springframework.security.web.server.WebFilterExchange;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import org.springframework.web.server.ServerWebExchange;
 
+import com.gw.JobApplicationTracker.service.CustomUserDetailsService;
+
 import java.net.URI;
 import java.nio.file.attribute.UserPrincipal;
 
 import reactor.core.publisher.Mono;
 
 public class JwtAuthenticationSuccessHandler implements ServerAuthenticationSuccessHandler {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationSuccessHandler.class);
 
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
@@ -24,6 +30,8 @@ public class JwtAuthenticationSuccessHandler implements ServerAuthenticationSucc
 
     @Override
     public Mono<Void> onAuthenticationSuccess(WebFilterExchange exchange, Authentication authentication) {
+
+        logger.warn(authentication.toString());
 
         String token = jwtTokenProvider.generateToken((com.gw.JobApplicationTracker.model.UserPrincipal) authentication.getPrincipal());
         exchange.getExchange().getResponse().getHeaders().add(HttpHeaders.AUTHORIZATION, "Bearer " + token);
